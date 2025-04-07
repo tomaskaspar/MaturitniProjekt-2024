16a588282c55611d28fac20c87b4e6369cfef4a0
tomaskaspar <tomas.kaspar13@gmail.com>
2025-04-07 12:06:05 +0200

    Update rezimyMotoru.ino

--- rezimyMotoru.ino
+++ rezimyMotoru.ino
@@ -9,143 +9,125 @@ int joystickPin = A1;  // Analogový pin pro joystick
 int motorSpeed = 100;  // Výchozí rychlost motorů (v rozsahu 70-220)
 
 // Tlačítka
-int button1 = 2; // První tlačítko
-int button2 = 3; // Druhé tlačítko
-int button3 = 4; // Třetí tlačítko
+int button1 = 2;
+int button2 = 3;
+int button3 = 4;
 
-int stav1 = LOW; // První tlačítko
-int btnstatepred1;// První tlačítko
-
-int stav2 = LOW; // Druhe tlačítko
-int btnstatepred2;// Druhe tlačítko
-
-int stav3 = LOW; // Druhe tlačítko
-int btnstatepred3;// Druhe tlačítko
-
+int stav1 = LOW;
+int btnstatepred1;
+int stav2 = LOW;
+int btnstatepred2;
+int stav3 = LOW;
+int btnstatepred3;
 
 void setup() {
-  // Nastavení pinů jako výstupy
+  Serial.begin(9600);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(enA, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
   pinMode(enAA, OUTPUT);
-
-  // Nastavení pinů pro tlačítka jako vstupy
-  pinMode(button1, INPUT_PULLUP); // Aktivní při spojení s GND
+  pinMode(button1, INPUT_PULLUP);
   pinMode(button2, INPUT_PULLUP);
   pinMode(button3, INPUT_PULLUP);
-
-
 }
 
 void loop() {
-  Serial.begin(9600);
-  //tlacitka stavy
   int btnvalue1 = digitalRead(button1);  
   int btnvalue2 = digitalRead(button2); 
   int btnvalue3 = digitalRead(button3);
+
   if (btnvalue1 == LOW && btnstatepred1 == HIGH) {
-      stav2=LOW;
-      stav3=LOW;
-      stav1 = !stav1;
-      Serial.println(stav1);
-    }
-
-   if (btnvalue2 == LOW && btnstatepred2 == HIGH) {
-      stav1 =LOW;
-      stav3 =LOW;
-      stav2 = !stav2;
-     
-      Serial.println(stav2);
-    }
-
-     if (btnvalue3 == LOW && btnstatepred3 == HIGH) {
-      stav2=LOW;
-      stav1=LOW;
-      stav3 = !stav3;
-      Serial.println(stav3);
-    }
-
-    
-
-  // Čtení hodnoty joysticku
-  int joystickValue = analogRead(joystickPin);
-
-  // Výpočet rychlosti motorů na základě joysticku
-  if (joystickValue > 600) {
-    motorSpeed += 5;
-    if (motorSpeed > 220) motorSpeed = 220; // Max rychlost 220
-  } else if (joystickValue < 400) {
-    motorSpeed -= 5;
-    if (motorSpeed < 40) motorSpeed = 40; // Min rychlost 70
+    stav2 = LOW;
+    stav3 = LOW;
+    stav1 = !stav1;
+    Serial.println(stav1);
   }
 
-  // Kontrola stavu tlačítek
+  if (btnvalue2 == LOW && btnstatepred2 == HIGH) {
+    stav1 = LOW;
+    stav3 = LOW;
+    stav2 = !stav2;
+    Serial.println(stav2);
+  }
+
+  if (btnvalue3 == LOW && btnstatepred3 == HIGH) {
+    stav2 = LOW;
+    stav1 = LOW;
+    stav3 = !stav3;
+    Serial.println(stav3);
+  }
+
+  int joystickValue = analogRead(joystickPin);
+
+  if (joystickValue > 600) {
+    motorSpeed += 5;
+    if (motorSpeed > 220) motorSpeed = 225;
+  } else if (joystickValue < 400) {
+    motorSpeed -= 5;
+    if (motorSpeed < 40) motorSpeed = 40;
+  }
+
   if (stav1 == HIGH) {
     sameDirection();
   } else if (stav2 == HIGH) {
-    delay(2000); // Pauza 2 sekundy
+   
     reverseMotor1();
-  } else if (stav3== HIGH) {
+  } else if (stav3 == HIGH) {
     reverseMotor2();
-  } 
-  
-  //Motory zastaveny
-  else {
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-
-  analogWrite(enA, 0);
-  analogWrite(enAA, 0);
-
-  Serial.println("Motory zastaveny");
+  } else {
+    stopMotors();
   }
 
-  
-  btnstatepred1 = digitalRead(button1);
-    btnstatepred2 = digitalRead(button2);
-      btnstatepred3 = digitalRead(button3);
-  delay(100); // Krátké zpoždění
+  btnstatepred1 = btnvalue1;
+  btnstatepred2 = btnvalue2;
+  btnstatepred3 = btnvalue3;
+  delay(100);
 }
 
-// Funkce pro stejný směr motorů
 void sameDirection() {
+  Serial.println("Motory se točí stejným směrem");
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
-
   analogWrite(enA, motorSpeed);
   analogWrite(enAA, motorSpeed);
-
-  Serial.println("Motory se točí stejným směrem");
+  Serial.print("motorSpeed: ");
+  Serial.println(motorSpeed);
 }
 
-// Funkce pro opačný směr motoru 1
 void reverseMotor1() {
+  Serial.println("Motor 1 opačný směr");
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
-
-analogWrite(enA, motorSpeed);
-analogWrite(enAA, max(motorSpeed - 30, 0)); 
-
-  Serial.println("Motor 1 opačný směr");
+  analogWrite(enA, motorSpeed);
+  analogWrite(enAA, max(motorSpeed - 30, 40));
+  Serial.print("motorSpeed: ");
+  Serial.println(motorSpeed);
 }
 
-// Funkce pro opačný směr motoru 2
 void reverseMotor2() {
-    Serial.println("Rotace dolu,levy motor z pohledu kratsi strany");
+  Serial.println("Rotace dolů, levý motor z pohledu kratší strany");
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
-  
-    analogWrite(enA, max(motorSpeed - 30, 0)); // Zajištění minimální hodnoty 0
-    analogWrite(enAA, motorSpeed);
-  }
+  analogWrite(enA, max(motorSpeed - 30, 40));
+  analogWrite(enAA, motorSpeed);
+  Serial.print("motorSpeed: ");
+  Serial.println(motorSpeed);
+}
+
+void stopMotors() {
+  Serial.println("Motory zastaveny");
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  analogWrite(enA, 0);
+  analogWrite(enAA, 0);
+}
